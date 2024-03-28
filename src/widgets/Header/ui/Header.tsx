edba0a02ab52133e7c0dcb2023be65a6b5693291
Anastasia Morozova <anafrost1993@gmail.com>
2024-03-28 13:20:27 +0700

    feat: added component Header

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import { LogoIcon, MoonIcon, SunIcon } from '@icons';
+
+import { Container } from '@common/Container';
+import { HeaderEl, ThemeSwitcher, Title, Wrapper, WrapperLogoTitle } from './Header.styled';
+
+export const Header = () => {
+	const [theme, setTheme] = useState('dark');
+
+	const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
+
+	useEffect(() => {
+		document.body.setAttribute('data-theme', theme);
+	}, [theme]);
+
+	return (
+		<HeaderEl>
+			<Container>
+				<Wrapper>
+					<WrapperLogoTitle>
+						<Title>High paw</Title>
+						{<LogoIcon />}
+					</WrapperLogoTitle>
+					<ThemeSwitcher onClick={toggleTheme}>
+						{theme === 'dark' ? <MoonIcon /> : <SunIcon />}
+						<span style={{ marginLeft: '8px' }}>{theme} Theme</span>
+					</ThemeSwitcher>
+				</Wrapper>
+			</Container>
+		</HeaderEl>
+	);
+};
