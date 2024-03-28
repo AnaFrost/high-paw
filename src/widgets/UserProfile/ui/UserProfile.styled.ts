5540c86a033cbd8b79c4aa05e78a0c45c032f6dd
Anastasia Morozova <anafrost1993@gmail.com>
2024-03-28 19:54:20 +0700

    feat: added UserProfile, User, UserAvatar,UserInfo components

---
+++ src/widgets/UserProfile/ui/UserProfile.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const UserProfileEl = styled.div`
+	background-color: var(--colors-bg);
+	width: 100%;
+	max-width: 880px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 40px 0 40px;
+`;
